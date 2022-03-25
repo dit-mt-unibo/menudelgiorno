d7f3d800d7ccadb87fd879992924948abc660d2e
Giulia Maldini <giuliamaldini@gmail.com>
2022-03-25 16:40:06 +0100

    creazione tabelle, modelli, controller, popolamento tabelle db con seeder

--- laravel_api/database/migrations/2022_03_25_110351_create_language_restaurant_table.php
+++ laravel_api/database/migrations/2022_03_25_110351_create_language_restaurant_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateLanguageRestaurantTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('language_restaurant', function (Blueprint $table) {
             $table->id();
-            $table->string('user');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('restaurant_id');
+            $table->integer('language_id');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('restaurant_language');
     }
 }
