fae4a921d04db2b69d33b1a4c7380049d5bdea9d
lytem <lytem85@gmail.com>
2022-03-26 14:04:20 +0100

    .

--- laravel_api/app/Http/Controllers/MenuController.php
+++ laravel_api/app/Http/Controllers/MenuController.php
@@ -9,25 +9,13 @@ use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $menu=Menu::get();
         return response()->json(['menu trovati',$menu]);
     }
 
-
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(MenuRequest $request,Menu $menu)
     {
         // $menu=new menu();
@@ -42,12 +30,7 @@ class MenuController extends Controller
         return response()->json(['menu creato',new MenuResource($menu)]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         $menu=Menu::find($id);
@@ -58,21 +41,7 @@ class MenuController extends Controller
         return response()->json(new MenuResource($menu));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
 
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(MenuRequest $request, Menu $menu)
     {
         $menu->name=$request->name;
@@ -82,12 +51,7 @@ class MenuController extends Controller
         return response()->json(['menu aggiornato',new MenuResource($menu)]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Menu $menu)
     {
         $menu->delete();
