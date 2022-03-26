19cf8d1f8224bcf5545fb67ef0962df5d0455e80
lytem <lytem85@gmail.com>
2022-03-26 18:55:17 +0100

    create api translation aggiornament api menu

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\MenuController;
 use App\Http\Controllers\Api\LanguageController;
+use App\Http\Controllers\Api\TranslationController;
 use App\Http\Controllers\Api\RegistryController;
 use App\Http\Controllers\Api\RestaurantController;
 
@@ -27,6 +28,7 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::apiResource('registries', RegistryController::class);
     Route::apiResource('languages', LanguageController::class);
     Route::apiResource('restaurants', RestaurantController::class);
+    Route::apiResource('translations', TranslationController::class);
     Route::apiResource('menus', MenuController::class);
     Route::post('auth/logout', [AuthController::class, 'logout']);
 });
