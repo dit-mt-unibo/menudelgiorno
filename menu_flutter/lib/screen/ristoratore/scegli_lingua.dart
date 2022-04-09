bf0bebaf19d19b15191c4e757597503eb36382ab
DavidCalin <81362932+DavidCalin@users.noreply.github.com>
2022-04-09 18:21:33 +0200

    lingua ristoratore

--- menu_flutter/lib/screen/ristoratore/scegli_lingua.dart
+++ menu_flutter/lib/screen/ristoratore/scegli_lingua.dart
@@ -1,125 +1,90 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'ristoratore_home.dart';
+import '../../models/app/language.dart';
 
 class ScegliLinguaWidget extends StatefulWidget {
-  const ScegliLinguaWidget({Key? key}) : super(key: key);
+  const ScegliLinguaWidget({Key? key}): super(key: key);
+
 
   @override
   State<ScegliLinguaWidget> createState() => _ScegliLinguaWidgetState();
 }
 
 class _ScegliLinguaWidgetState extends State<ScegliLinguaWidget> {
+  
+  Future<Language> _getLanguage() async {
+    final url = Uri.http('10.0.2.2:8000', '/api/languages/');
+    final response = await http.get(url);
+    final data = jsonDecode(response.body);
+    final languages = Language.fromJson(data);
+    print(languages);
+    return languages;
+  }
+
+  bool italiano = false;
   bool inglese = false;
   bool francese = false;
   bool tedesco = false;
   bool spagnolo = false;
   bool portoghese = false;
-  bool russo = false;
   bool cinese = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Seleziona una o più lingue',
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          CheckboxListTile(
-            value: inglese,
-            onChanged: (val) {
-              setState(() {
-                inglese = val as bool;
-              });
-            },
-            activeColor: Colors.green,
-            title: const Text('Inglese'),
-          ),
-          CheckboxListTile(
-            value: francese,
-            onChanged: (val) {
-              setState(() {
-                francese = val as bool;
-              });
-            },
-            activeColor: Colors.green,
-            title: const Text('Francese'),
-          ),
-          CheckboxListTile(
-            value: tedesco,
-            onChanged: (val) {
-              setState(() {
-                tedesco = val as bool;
-              });
-            },
-            activeColor: Colors.green,
-            title: const Text('Tedesco'),
-          ),
-          CheckboxListTile(
-            value: spagnolo,
-            onChanged: (val) {
-              setState(() {
-                spagnolo = val as bool;
-              });
-            },
-            activeColor: Colors.green,
-            title: const Text('Spagnolo'),
-          ),
-          CheckboxListTile(
-            value: portoghese,
-            onChanged: (val) {
-              setState(() {
-                portoghese = val as bool;
-              });
-            },
-            activeColor: Colors.green,
-            title: const Text('Portoghese'),
-          ),
-          CheckboxListTile(
-            value: russo,
-            onChanged: (val) {
-              setState(() {
-                russo = val as bool;
-              });
-            },
-            activeColor: Colors.green,
-            title: const Text('Russo'),
-          ),
-          CheckboxListTile(
-            value: cinese,
-            onChanged: (val) {
-              setState(() {
-                cinese = val as bool;
-              });
-            },
-            activeColor: Colors.green,
-            title: const Text('Cinese'),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              fixedSize: const Size(150, 40),
-            ),
-            child: const Text(
-              'Conferma',
-              style: TextStyle(
-                fontSize: 19,
-              ),
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const RistoratoreHome(),
+      body: FutureBuilder(
+        future: _getLanguage(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final language = snapshot.data as Language;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Seleziona una o più lingue',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
-              );
-            },
-          ),
-        ],
+                CheckboxListTile(
+                  value: inglese,
+                  onChanged: (val) {
+                    setState(() {
+                      inglese = val as bool;
+                    });
+                  },
+                  activeColor: Colors.green,
+                  title: Text(language.name),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(150, 40),
+                  ),
+                  child: const Text(
+                    'Conferma',
+                    style: TextStyle(
+                      fontSize: 19,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const RistoratoreHome(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
