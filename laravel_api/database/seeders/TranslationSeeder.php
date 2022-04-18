0cbf8a178ecc7c93c01e0eb5d3961507b6616395
lytem <lytem85@gmail.com>
2022-04-19 01:23:16 +0200

    translationseeder

--- laravel_api/database/seeders/TranslationSeeder.php
+++ laravel_api/database/seeders/TranslationSeeder.php
@@ -12,7 +12,7 @@ class TranslationSeeder extends Seeder
         DB::table('translations')->insert([
             'language_id' => '2',
             'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur id mi non enim suscipit ornare ut id lorem.Sed ornare odio nec consequat mollis.Vivamus nec urna eu augue interdum volutpat nec non lectus.Vestibulum eu ipsum nec mi condimentum convallis.',
-            'menu_id' => '2',
+            'menu_id' => '1',
             'state' => '0',
             'user_id' => null, // Id utente traduttore
             'created_at' => now(),
@@ -20,7 +20,7 @@ class TranslationSeeder extends Seeder
         ]);
 
         DB::table('translations')->insert([
-            'language_id' => '2',
+            'language_id' => '3',
             'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur id mi non enim suscipit ornare ut id lorem.Sed ornare odio nec consequat mollis.Vivamus nec urna eu augue interdum volutpat nec non lectus.Vestibulum eu ipsum nec mi condimentum convallis.',
             'menu_id' => '1',
             'state' => '0',
@@ -32,7 +32,7 @@ class TranslationSeeder extends Seeder
         DB::table('translations')->insert([
             'language_id' => '4',
             'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur id mi non enim suscipit ornare ut id lorem.Sed ornare odio nec consequat mollis.Vivamus nec urna eu augue interdum volutpat nec non lectus.Vestibulum eu ipsum nec mi condimentum convallis.',
-            'menu_id' => '3',
+            'menu_id' => '2',
             'state' => '1',
             'user_id' => '4', // Id utente traduttore
             'created_at' => now(),
@@ -42,7 +42,7 @@ class TranslationSeeder extends Seeder
         DB::table('translations')->insert([
             'language_id' => '6',
             'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur id mi non enim suscipit ornare ut id lorem.Sed ornare odio nec consequat mollis.Vivamus nec urna eu augue interdum volutpat nec non lectus.Vestibulum eu ipsum nec mi condimentum convallis.',
-            'menu_id' => '5',
+            'menu_id' => '2',
             'state' => '1',
             'user_id' => '4', // Id utente traduttore
             'created_at' => now(),
@@ -50,9 +50,27 @@ class TranslationSeeder extends Seeder
         ]);
 
         DB::table('translations')->insert([
-            'language_id' => '7',
+            'language_id' => '1',
             'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur id mi non enim suscipit ornare ut id lorem.Sed ornare odio nec consequat mollis.Vivamus nec urna eu augue interdum volutpat nec non lectus.Vestibulum eu ipsum nec mi condimentum convallis.',
-            'menu_id' => '4',
+            'menu_id' => '2',
+            'state' => '0',
+            'user_id' => null, // Id utente traduttore
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('translations')->insert([
+            'language_id' => '3',
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur id mi non enim suscipit ornare ut id lorem.Sed ornare odio nec consequat mollis.Vivamus nec urna eu augue interdum volutpat nec non lectus.Vestibulum eu ipsum nec mi condimentum convallis.',
+            'menu_id' => '3',
+            'state' => '0',
+            'user_id' => null, // Id utente traduttore
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('translations')->insert([
+            'language_id' => '1',
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur id mi non enim suscipit ornare ut id lorem.Sed ornare odio nec consequat mollis.Vivamus nec urna eu augue interdum volutpat nec non lectus.Vestibulum eu ipsum nec mi condimentum convallis.',
+            'menu_id' => '3',
             'state' => '0',
             'user_id' => null, // Id utente traduttore
             'created_at' => now(),
