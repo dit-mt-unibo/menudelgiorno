11b57b2c394444aed2862d8adfb354a051cb7c3d
lytem <lytem85@gmail.com>
2022-04-19 01:25:18 +0200

    translationseeder

--- laravel_api/database/seeders/TranslationSeeder.php
+++ laravel_api/database/seeders/TranslationSeeder.php
@@ -38,6 +38,15 @@ class TranslationSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]);
+        DB::table('translations')->insert([
+            'language_id' => '6',
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur id mi non enim suscipit ornare ut id lorem.Sed ornare odio nec consequat mollis.Vivamus nec urna eu augue interdum volutpat nec non lectus.Vestibulum eu ipsum nec mi condimentum convallis.',
+            'menu_id' => '2',
+            'state' => '0',
+            'user_id' => null, // Id utente traduttore
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
 
         DB::table('translations')->insert([
             'language_id' => '6',
